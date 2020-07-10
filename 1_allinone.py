b7613febeef29d4baf65aacda8b54d25ec4c3f67
Murat Açıkgöz <muraci@gmail.com>
2020-07-10 23:23:34 +0300

    Add files via upload

---
+++ 1_allinone.py
@@ -0,0 +1,61 @@
+class explorer:
+	def describe(df):
+	#count,mean,std,min,max,null,dtype,zero_,nunique,freq,top,upout_,lowout_
+	
+	def unique(df):
+	#int or obj &  >100>
+	
+	def winsorize(df):
+    #upout_ or lowout_ winsorize
+
+	def quantile(df):
+	#25,50,75
+	
+	def splitby(df):
+	#directly proportional
+
+
+	
+class visualizer:
+	def outlier(df):
+	#minmax > boxplot
+
+	def bartext(df):
+	#bar height annotate
+	
+	def multiscatter(df):
+	#rows and columns
+
+	def stackbar(df):
+	#stacked bar plot
+	
+	def mapplot(df):
+	#folium map
+	
+	def stdbar(col):
+	#col based std and bar plot
+
+
+	
+class learner:
+	def regframe(X,y):
+	#rsq,mae,mse,rmse,mape
+	
+	def predplot(X,y):
+	#multi pred plot
+	
+	def confusion(X,y):
+	#confusion matrix, precision, recall, fscore
+	
+	def parameter(param):
+	#best params
+	
+	def modelframe(X,y):
+	#accuracy, precision, recall, f1-score, auc
+	
+	def modelcurve(X,y):
+	#roc and recall/precision
+	
+	def modelcoef(X,y):
+	#coefficient bar plot
+	
