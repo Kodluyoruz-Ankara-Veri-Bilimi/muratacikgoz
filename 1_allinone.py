aec7aea55b1c1282bd54e4115a593427b2e03d1f
Murat Açıkgöz <muraci@gmail.com>
2020-07-23 17:48:46 +0300

    Update 1_allinone.py

--- 1_allinone.py
+++ 1_allinone.py
@@ -128,3 +128,100 @@ def nullbar(df):
 
 #def modelcoef(X,y):
 ##coefficient bar plot
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from statsmodels.tools.eval_measures import mse, rmse
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import statsmodels.api as sm
+
+
+def perfstats(col, Y):
+    pf = pd.DataFrame(columns=['model', 'rsq', 'rsq_adj', 'f_value', 'aic', 'bic', 'mae', 'mse', 'rmse', 'mape'])
+    pd.options.display.float_format = '{:.3f}'.format
+    for num,X in enumerate(col,1): 
+        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+        
+        standardscaler = StandardScaler()
+        x_train = standardscaler.fit_transform(x_train)
+        x_test = standardscaler.transform(x_test)
+        
+        x_train = sm.add_constant(x_train)
+        results = sm.OLS(y_train, x_train).fit()
+        x_test = sm.add_constant(x_test)
+        y_pred = results.predict(x_test)
+        pf.loc[num] = ('model_'+str(num) , results.rsquared, results.rsquared_adj, results.fvalue, results.aic, results.bic, 
+                       mean_absolute_error(y_test, y_pred), mse(y_test, y_pred), rmse(y_test, y_pred), (np.mean(np.abs((y_test - y_pred) / y_test)) * 100))
+    return pf
+
+def predplts(col, Y):
+    if(len(col) % 3) == 0:
+        row = int(len(col) / 3)
+    elif (len(col) % 3) != 0:
+        row = int((len(col) // 3) +1)
+    for num,X in enumerate(col,1): 
+        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+        
+        standardscaler = StandardScaler()
+        x_train = standardscaler.fit_transform(x_train)
+        x_test = standardscaler.transform(x_test)
+        
+        results = LinearRegression().fit(x_train, y_train)
+        y_pred = results.predict(x_test)
+        
+        plt.subplot(row, 3, num)
+        sns.scatterplot(x=y_test, y=y_pred)
+        sns.lineplot(x=y_test, y=y_test, label='ytest')
+        plt.ylabel("predict")
+        plt.title('model_'+str(num))
+        plt.tight_layout()
+    return 
+
+def regstats(col, Y):  #'linear',
+    
+    pf = pd.DataFrame(columns=['model', 'rsq_train', 'rsq_test', 'subt_rsq', 'mae_test', 'mse_test', 'rmse_test', 'mape_test']) 
+    pd.options.display.float_format = '{:.3f}'.format
+    
+    
+    for num,X in enumerate(col,1): 
+        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+        
+        standardscaler = StandardScaler()
+        x_train = standardscaler.fit_transform(x_train)
+        x_test = standardscaler.transform(x_test)
+        
+        
+        results = LinearRegression().fit(x_train, y_train)
+        y_pred = results.predict(x_test)
+        
+        pf.loc[num] = ('model_'+str(num) ,
+                       results.score(x_train, y_train),
+                       results.score(x_test, y_test),
+                       results.score(x_train, y_train) - results.score(x_test, y_test),
+                       mean_absolute_error(y_test, y_pred), 
+                       mse(y_test, y_pred), 
+                       rmse(y_test, y_pred), 
+                       (np.mean(np.abs((y_test - y_pred) / y_test)) * 100))
+    return pf
+
+def coefplts(X2,Y2):
+    x_train, x_test, y_train, y_test = train_test_split(X2, Y2, test_size = 0.2, random_state = 42)
+    
+    standardscaler = StandardScaler()
+    x_train = standardscaler.fit_transform(x_train)
+    x_test = standardscaler.transform(x_test)
+    
+    model1 = LinearRegression().fit(x_train, y_train)
+    dfCoef = pd.DataFrame([model1.coef_[0:]], columns=X2.columns, index=['Linear']).T
+    
+    ax = dfCoef.plot.bar(rot=0, figsize=(15, 4), width=0.9)
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    for i, p in enumerate(ax.patches):
+        ax.annotate("%.2f" % p.get_height(), (p.get_x() + p.get_width() / 2, p.get_height()),
+                         ha='center', va='center', rotation=90, xytext=(0, 20), textcoords='offset points')
+    plt.ylim(-4, 14)
+    plt.tight_layout()
+    plt.show()
+    return
