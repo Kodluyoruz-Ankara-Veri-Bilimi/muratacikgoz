e44fc50d45bcaeb3407f93dd7a8ef92e4af89762
Murat Açıkgöz <muraci@gmail.com>
2020-08-03 19:21:02 +0300

    Update 1_allinone.py

--- 1_allinone.py
+++ 1_allinone.py
@@ -5,21 +5,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+#-----------------------------------------
 
 def describe(df):
 
-    dfinfo = pd.DataFrame(columns=['dtype','count','mean','std','min','median','max','dupe','null','zero','unique','freq','top','upout','lowout'])
+    dfinfo = pd.DataFrame(columns=['dtype','count','mean','std','min','median','max','null','zero','unique','freq','top','upout','lowout'])
 
     for col in df.columns:
         if df[col].dtypes != object:
             dfinfo.loc[col,'dtype'] = df[col].dtypes
-            dfinfo.loc[col,'count'] =  ('%.0f' % df[col].count())  
+            dfinfo.loc[col,'count'] =  ('%.0f' % df[col].count())
             dfinfo.loc[col,'mean'] = df[col].mean()
             dfinfo.loc[col,'std'] = df[col].std()
             dfinfo.loc[col,'min'] = df[col].min()
             dfinfo.loc[col,'median'] = df[col].median()
             dfinfo.loc[col,'max'] = df[col].max()
-            dfinfo.loc[col,'dupe'] = ('%.0f' % len(df[df.duplicated()]))
             dfinfo.loc[col,'null'] = ('%.0f' % df[col].isnull().sum())
             dfinfo.loc[col,'zero'] = ('{:.3%}'.format(df[df[col] == 0][col].count() / len(df[col])))
             dfinfo.loc[col,'unique'] = ('%.0f' % df[col].nunique())
@@ -33,8 +35,7 @@ def describe(df):
         
         elif df[col].dtypes == object:
             dfinfo.loc[col,'dtype'] = df[col].dtypes
-            dfinfo.loc[col,'count'] =  ('%.0f' % df[col].count())  
-            dfinfo.loc[col,'dupe'] = ('%.0f' % len(df[df.duplicated()]))
+            dfinfo.loc[col,'count'] =  ('%.0f' % df[col].count())
             dfinfo.loc[col,'null'] = df[col].isnull().sum()
             dfinfo.loc[col,'zero'] = ('{:.3%}'.format(df[df[col] == 0][col].count() / len(df[col])))
             dfinfo.loc[col,'unique'] = df[col].nunique()
@@ -43,6 +44,8 @@ def describe(df):
 
     return dfinfo
 
+
+#-----------------------------------------
 
 def unique(df, dtype = 'all', value = 100):
 
@@ -63,34 +66,98 @@ def unique(df, dtype = 'all', value = 100):
     return 
 
 
+#-----------------------------------------
+
+from scipy.stats.mstats import winsorize
+
+def winsorized(df, col):
+    
+    ##upout or lowout winsorize
+
+    Q1,Q3=df[col].quantile(q=0.25),df[col].quantile(q=0.75)
+    lowOutliers,upperOutliers=Q1-1.5*(Q3-Q1),Q3+1.5*(Q3-Q1)
+    upout = (df[df[col]>upperOutliers][col].count()*100/len(df))
+    lowout = (df[df[col]<lowOutliers][col].count()*100/len(df))
+    if (upout != 0) | (lowout != 0):
+        df[col] = winsorize(df[col], ((lowout / 100) + 1e-5, (upout / 100) + 1e-5)) #alt-üst
+    
+    return df
 
 
-#def winsorized(df):
-##upout_ or lowout_ winsorize
+#-----------------------------------------
 
-#def quantile(df):
-##25,50,75
+def quantile(df, col):
 
-#def splitby(df):
-##directly proportional
+    ##25,50,75
+
+    q25, q50, q75 = df[col].quantile(0.25), df[col].quantile(0.50), df[col].quantile(0.75)
+    df.loc[(df[col] <= q25), str('q_')+col] = 0
+    df.loc[(df[col] > q25) & (df[col] <= q50), str('q_')+col] = 1
+    df.loc[(df[col] > q50) & (df[col] <= q75), str('q_')+col] = 2
+    df.loc[(df[col] > q75), str('q_')+col] = 3
+    
+    return df
 
 
+#-----------------------------------------
+
+def splitby(df, col, by, unknown):
+
+    ##directly proportional / for col 2 unique
+
+    crossTab = pd.crosstab(df[by], df[col])
+    byValue = df[by].unique()
+    for val in byValue:
+        idx = df.loc[(df[col] == unknown) & (df[by] == val)].index
+        mask = np.random.rand(len(idx)) < ((crossTab.loc[by][df[col].unique()[0]]) / (crossTab.loc[by][df[col].unique()[1]] + crossTab.loc[by][df[col].unique()[1]]))
+        idx_no, idx_yes = idx[mask], idx[~mask]
+        df.loc[idx_no, col], df.loc[idx_yes, col] = df[col].unique()[0], df[col].unique()[1]
+    
+    return df
+
+
+#-----------------------------------------
 
 ###visualizer:
 
 def nullbar(df):
-## null value percent
+
+    ## null value percent
+    
     for col in df.columns:
         plt.barh(col, len(df[df[col].notna()])/len(df), color = '#348ABD')
         plt.barh(col, len(df[df[col].isna()])/len(df), left = len(df[df[col].notna()])/len(df), color = '#E24A33')
     
     return plt.show()
 
-#def outlier(df):
-##minmax > boxplot
 
-#def bartext(df):
-##bar height annotate
+#-----------------------------------------
+
+def outlierbox(df):
+    
+    ##minmax > boxplot
+    
+    dfNorm = pd.DataFrame(MinMaxScaler().fit_transform(df), columns=df.columns)
+    sns.boxplot(data = dfNorm, palette='Set3', width = 0.6)
+    
+    return plt.show()
+
+
+#-----------------------------------------
+
+def textbar(df):
+
+    ##bar height annotate
+    
+    ax = df.plot.bar(rot=0, figsize=(15, 4), width=0.9)
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    for i, p in enumerate(ax.patches):
+        ax.annotate("%.2f" % p.get_height(), (p.get_x() + p.get_width() / 2, p.get_height()),
+                         ha='center', va='center', rotation=90, xytext=(0, 20), textcoords='offset points')
+
+    return plt.show()
+
+#-----------------------------------------
 
 #def multiscatter(df):
 ##rows and columns
@@ -108,37 +175,18 @@ def nullbar(df):
 
 ###learner
 
-#def regframe(X,y):
-##rsq,mae,mse,rmse,mape
-
-#def predplot(X,y):
-##multi pred plot
-
-#def confusion(X,y):
-##confusion matrix, precision, recall, fscore
-
-#def parameter(param):
-##best params
-
-#def modelframe(X,y):
-##accuracy, precision, recall, f1-score, auc
-
-#def modelcurve(X,y):
-##roc and recall/precision
-
-#def modelcoef(X,y):
-##coefficient bar plot
-
-
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from statsmodels.tools.eval_measures import mse, rmse
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import statsmodels.api as sm
 
+#-----------------------------------------
 
 def perfstats(col, Y):
+
+    ##rsq,mae,mse,rmse,mape
+
     pf = pd.DataFrame(columns=['model', 'rsq', 'rsq_adj', 'f_value', 'aic', 'bic', 'mae', 'mse', 'rmse', 'mape'])
     pd.options.display.float_format = '{:.3f}'.format
     for num,X in enumerate(col,1): 
@@ -156,7 +204,13 @@ def perfstats(col, Y):
                        mean_absolute_error(y_test, y_pred), mse(y_test, y_pred), rmse(y_test, y_pred), (np.mean(np.abs((y_test - y_pred) / y_test)) * 100))
     return pf
 
+
+#-----------------------------------------
+
 def predplts(col, Y):
+
+    ##multi pred plot
+
     if(len(col) % 3) == 0:
         row = int(len(col) / 3)
     elif (len(col) % 3) != 0:
@@ -177,10 +231,16 @@ def predplts(col, Y):
         plt.ylabel("predict")
         plt.title('model_'+str(num))
         plt.tight_layout()
-    return 
-
-def regstats(col, Y):  #'linear',
     
+    return 
+    
+    
+#-----------------------------------------
+
+def regstats(col, Y):  #linear
+
+    ##rsq,mae,mse,rmse,mape
+
     pf = pd.DataFrame(columns=['model', 'rsq_train', 'rsq_test', 'subt_rsq', 'mae_test', 'mse_test', 'rmse_test', 'mape_test']) 
     pd.options.display.float_format = '{:.3f}'.format
     
@@ -206,7 +266,13 @@ def regstats(col, Y):  #'linear',
                        (np.mean(np.abs((y_test - y_pred) / y_test)) * 100))
     return pf
 
+
+#-----------------------------------------
+
 def coefplts(X2,Y2):
+
+    ##coefficient bar plot
+
     x_train, x_test, y_train, y_test = train_test_split(X2, Y2, test_size = 0.2, random_state = 42)
     
     standardscaler = StandardScaler()
@@ -224,4 +290,20 @@ def coefplts(X2,Y2):
     plt.ylim(-4, 14)
     plt.tight_layout()
     plt.show()
+    
     return
+    
+    
+#-----------------------------------------
+
+#def confusion(X,y):
+##confusion matrix, precision, recall, fscore
+
+#def parameter(param):
+##best params
+
+#def modelframe(X,y):
+##accuracy, precision, recall, f1-score, auc
+
+#def modelcurve(X,y):
+##roc and recall/precision
